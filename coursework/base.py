00d1f8e7717445ce037360de3c69a712fd1e0720
MyMac <benjaminsbrown98@gmail.com>
2018-10-19 18:09:05 +0100

    .

--- coursework/base.py
+++ coursework/base.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 def root():
 	return render_template('home.html'), 200
 @app.route('/romans.html')
-def root():
+def display():
 	return render_template('romans.html'), 200
-	
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
