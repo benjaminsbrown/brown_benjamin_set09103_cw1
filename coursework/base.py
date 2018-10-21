d2250f4b4afdb815274b06827264af7470077199
MyMac <benjaminsbrown98@gmail.com>
2018-10-21 03:04:29 +0100

    pantheon page link test

--- coursework/base.py
+++ coursework/base.py
@@ -4,23 +4,27 @@ app = Flask(__name__)
 @app.route('/')
 def root():
 	return render_template('home.html'), 200
-
-
+@app.route('/demigods/')
+def demigods():
+	return render_template('demigods.html'), 200
+@app.route('/demigods.html')
+def demigodshtml():
+	return render_template('demigods.html'), 200
 @app.route('/greeks/')
 def greeks():
-	return render_template('greeks.html')
+	return render_template('greeks.html'), 200
 @app.route('/greeks/pantheon/')
 def pantheon():
-	return render_template('pantheon.html')
+	return render_template('pantheon.html'), 200
 @app.route('/romans/')
 def romans():
-	return render_template('romans.html')
+	return render_template('romans.html'), 200
 @app.route('/romans/romans/')
 def romans2():
-	return render_template('romans.html')
+	return render_template('romans.html'), 200
 @app.route('/romans.html')
 def romanshtml():
-	return render_template('romans.html')
+	return render_template('romans.html'), 200
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
