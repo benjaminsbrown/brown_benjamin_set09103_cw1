ff1798be8ef29d74da0294f0fd222c5ec7cfc1f4
MyMac <benjaminsbrown98@gmail.com>
2018-10-22 19:42:44 +0100

    links

--- coursework/base.py
+++ coursework/base.py
@@ -28,6 +28,9 @@ def romans():
 @app.route('/romans.html')
 def romanshtml():
 	return render_template('romans.html'), 200
+@app.route('/romans-gods.html')
+def romanshtml():
+	return render_template('romans.html'), 200
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
